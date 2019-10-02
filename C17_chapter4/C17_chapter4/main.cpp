81b1a843bb277128c79869e498c614e067e9980b
art lux <180116art@gmail.com>
2019-10-02 17:25:59 +0300

    Chapter 4 finalized

--- C17_chapter4/C17_chapter4/main.cpp
+++ C17_chapter4/C17_chapter4/main.cpp
@@ -4,6 +4,10 @@ extern void lambda_intro();
 extern void lambda_with_function();
 extern void make_function_by_concatenation();
 extern void complex_predicates_with_logic_conjunction();
+extern void different_func_with_same_incoming_data();
+extern void transform_if_by_accumulate_n_lambda();
+extern void cartesian_product_with_any_input_at_compile_time();
+
 int main()
 {
 	lambda_intro();
@@ -16,6 +20,15 @@ int main()
 	std::cout << std::endl;
 
 	complex_predicates_with_logic_conjunction();
+	std::cout << std::endl;
+
+	different_func_with_same_incoming_data();
+	std::cout << std::endl;
+
+	transform_if_by_accumulate_n_lambda();
+	std::cout << std::endl;
+
+	cartesian_product_with_any_input_at_compile_time();
 	std::cout << std::endl;
 	return 0;
 }
